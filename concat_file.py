fe535f89378570f4ac94a530166a70e2f2dbaf05
yao0510 <sf163864@gmail.com>
2021-07-16 12:45:29 +0800

    modify concat file

--- concat_file.py
+++ concat_file.py
@@ -18,6 +18,8 @@ def get_filename(folder):
         'court', 'date', 'no', 'sys', 'reason', 'judgement', 'attachAsJudgement', 'attachments', 'type', 'historyHash', 'mainText', 'opinion', 'relatedIssues', 'party'
     }
 
+    output_name = folder.split('/')[1] + '.csv'
+
     for idx, json_path in tqdm(enumerate(json_paths), total=len(json_paths)):
         with open(json_path) as json_data:
             data = json.load(json_data)
@@ -26,8 +28,21 @@ def get_filename(folder):
     
     all_json = pd.concat(all_json, ignore_index=True, sort=False)
     all_json['idx'] = all_json.index
-    all_json.to_csv('concat.csv', index=False)
+    all_json.to_csv(output_name, index=False)
 
 
 if __name__ == '__main__':
-    get_filename(sys.argv[1])
+    root_folder = sys.argv[1]
+    directory_list = []
+    count = 0
+    for root, dirs, files in os.walk(root_folder, topdown=False):
+        for name in dirs:
+            subfolder = os.path.join(root, name)
+            get_filename(subfolder)
+            
+            if count % 5 == 0:
+                print("Current: {}".format(count))
+            count += 1
+
+    
+    print("DONE")
