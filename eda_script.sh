95bf2c6ffab956eea7e50d07b80a2147dfa9396e
yao0510 <sf163864@gmail.com>
2021-08-04 20:22:41 +0800

    finish rule-based, but mae is about two years

--- eda_script.sh
+++ eda_script.sh
@@ -1,3 +1,3 @@
 # python concat_file.py $1
 # python eda.py $1
-python recommendation.py --filename filter_concat.csv --reason '殺人' --issue '中華民國刑法 272'
+python recommendation.py --filename new_truth_data.csv --reason '殺人' --issue '中華民國刑法 272' --TOPK 5
--- recommendation.py
+++ recommendation.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import argparse
 import os
+import numpy as np
 from ast import literal_eval
+from tqdm import tqdm
+from sklearn.metrics import mean_absolute_error
 
 
 def get_argument():
@@ -18,44 +21,95 @@ def get_argument():
                         type=str,
                         required=True,
                         help="query related issues")
+    opt.add_argument("--TOPK",
+                        type=int,
+                        required=True,
+                        help="Top K related")
     
     return opt
 
 
+def split_train_test(df):
+    df['year'] = df['date'].apply(lambda x: int(x.split('-')[0]))
+
+    train = df[df['year'] != 2020].reset_index(drop=True)
+    test = df[df['year'] == 2020].reset_index(drop=True)
+
+    print('Train size: {}, test size: {}'.format(len(train), len(test)))
+
+    # test.to_csv('test.csv', index=False)
+
+    return train, test
+
+
 def rule_based(data, query):
+    top_k = query['TOPK']
     match_indexes = []
+    same_issue_count = [0 for _ in range(len(data))]
     for idx in range(len(data)):
         if data['reason'][idx] == query['reason']:
             related_issues = literal_eval(data['relatedIssues'][idx])
-            split_query = query['issue'].split(' ')
-            for law in related_issues:
-                split_issues = law['issueRef'].split(' ')
-                if law['lawName'] == split_query[0] and law['issueRef'] == split_query[1]:
-                    match_indexes.append(idx)
-                    break
 
-    matches = data.loc[match_indexes]
-    matches.to_csv('results/rule_based_{}_{}.csv'.format(query['reason'], query['issue']), index=False)
+            for query_law in query['issue']:
+                for data_law in related_issues:
+                    if query_law['lawName'] == data_law['lawName'] and query_law['issueRef'] == data_law['issueRef']:
+                        same_issue_count[idx] += 1
+                        break
+
+    same_issue_count = np.array(same_issue_count)
+    non_zero_indexes = np.nonzero(same_issue_count)[0]
+
+    top_indexes = same_issue_count.argsort()[-top_k:][::-1]
+
+    # remove zero indexes
+    top_nonzero_indexes = []
+    for index in top_indexes:
+        if index not in non_zero_indexes:
+            top_nonzero_indexes.append(index)
+
+    # if len(top_nonzero_indexes) != len(top_indexes):
+    #     print("HI")
+
+    matches = data.loc[top_nonzero_indexes]
+    matches.to_csv('results/rule_based_{}.csv'.format(query['no']), index=False)
+    return matches
 
 
+def recommend_similar(train, test):
+    y_true, y_pred = [], []
+    for idx in tqdm(range(len(test))):
+        query = {
+            'no': test['no'][idx],
+            'reason': test['reason'][idx],
+            'issue': literal_eval(test['new_relatedIssues'][idx]),
+            'truth': test['truth'][idx],
+            'penalty': test['maxpenalty'][idx],
+            'TOPK': config['TOPK']
+        }
 
-def recommend_similar(filename, query):
-    df = pd.read_csv(filename)
-    print(query)
-    matches = rule_based(df, query)
+        matches = rule_based(train, query)
+
+        if len(matches) != 0:
+            y_pred.append(sum(matches['maxpenalty']) / len(matches['maxpenalty']))
+        else:
+            y_pred.append(0)
+        y_true.append(query['penalty'])
+
+    mae = mean_absolute_error(y_true, y_pred)
+    print("MAE : {}".format(mae))
 
 
 if __name__ == '__main__':
     opt = get_argument()
     config = vars(opt.parse_args())
 
+    # read data
     filename = config['filename']
-    query = {
-        'reason': config['reason'],
-        'issue': config['issue']
-    }
+    df = pd.read_csv(filename)
+
+    train, test = split_train_test(df)
 
     if not os.path.exists('results'):
         os.makedirs('results')
 
-    recommend_similar(filename, query)
+    recommend_similar(train, test)
