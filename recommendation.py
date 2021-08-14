718de7d1711e410ef5b0fd4f10b7f17cf943e61b
yao0510 <sf163864@gmail.com>
2021-08-14 17:50:34 +0800

    fixed version to related issues

--- recommendation.py
+++ recommendation.py
@@ -91,11 +91,22 @@ def using_bert(data, query):
     if not os.path.isfile('train_bert.pickle'):
         train_embedding = {}
         for idx in tqdm(range(len(data))):
-            if len(query['truth']) >= max_length:
+            # # process truth with tfidf results
+            # filter_truth = ''
+            # if idx > 475:
+            #     print(data['no'][idx])
+            #     print(data['truth'][idx])
+            # for word in data['truth'][idx].split(' '):
+            #     if word in data['parse_rank50'][idx]:
+            #         filter_truth += (' ' + word)
+            #     else:
+            #         filter_truth += (' ' + '<UNK>')
+
+            if len(data['truth'][idx]) >= max_length:
                 truth = data['truth'][idx][:max_length]
             else:
                 truth = data['truth'][idx]
-            outputs_truth = transform_embedding(truth)
+            outputs_truth = transform_embedding(truth)['pooler_output'][0].tolist()
 
             outputs_reason = transform_embedding(data['reason'][idx])['pooler_output'][0].tolist()
 
@@ -108,14 +119,21 @@ def using_bert(data, query):
                 else:
                     outputs_new_reason = [sum(x) for x in zip(outputs_new_reason, transform_embedding(reason)['pooler_output'][0].tolist())]
 
-            train_embedding[data['no'][idx]] = (outputs_reason, outputs_new_reason, outputs_truth['pooler_output'][0].tolist())
+            key_name = data['court'][idx] + '#' + data['no'][idx]
+            train_embedding[key_name] = (outputs_reason, outputs_new_reason, outputs_truth)
 
         with open('train_bert.pickle', 'wb') as file:
             pickle.dump(train_embedding, file)
 
     top_k = query['TOPK']
 
-    # get query embedding from BERT
+    # # get query embedding from BERT
+    # query_filter_truth = ''
+    # for word in query['truth'].split(' '):
+    #     if word in query['parse_rank']:
+    #         query_filter_truth += (' ' + word)
+    #     else:
+    #         query_filter_truth += (' ' + '<UNK>')
     if len(query['truth']) >= max_length:
         query['truth'] = query['truth'][:max_length]
     outputs_truth = transform_embedding(query['truth'])
@@ -124,7 +142,7 @@ def using_bert(data, query):
     query_reason_embedding = transform_embedding(query['reason'])['pooler_output'][0].tolist()
 
     # parse new reason by sections of law
-    new_reason = set(query['reason'])
+    new_reason = set(query['new_reason'])
     query_new_reason_embedding = None
     for reason in new_reason:
         if query_new_reason_embedding is None:
@@ -135,25 +153,42 @@ def using_bert(data, query):
     with open('train_bert.pickle', 'rb') as f:
         trained_embeddings = pickle.load(f)
 
+    same_issue_count = {}
+    for idx in range(len(data)):
+        related_issues = literal_eval(data['relatedIssues'][idx])
+
+        same_count = 0
+        for query_law in query['issue']:
+            for data_law in related_issues:
+                if query_law['lawName'] == data_law['lawName'] and query_law['issueRef'] == data_law['issueRef']:
+                    same_count += 1
+                    break
+        
+        key_name = data['court'][idx] + '#' + data['no'][idx]
+        same_issue_count[key_name] = (same_count / len(query['issue']))
+
     similarity_index = {}
     for no, trained_embedding in trained_embeddings.items():
         similarity_truth = 1 - spatial.distance.cosine(query_truth_embedding, trained_embedding[2])
         similarity_new_reason = 1 - spatial.distance.cosine(query_new_reason_embedding, trained_embedding[1])
         similarity_reason = 1 - spatial.distance.cosine(query_reason_embedding, trained_embedding[0])
-        similarity_index[no] = (similarity_truth + similarity_new_reason + similarity_reason)
+        similarity_issues = same_issue_count[no]
+        similarity_index[no] = similarity_truth + similarity_new_reason + similarity_reason + similarity_issues
     
     similarity_index = dict(sorted(similarity_index.items(), key=lambda item: item[1], reverse=True))
 
     top_indexes = []
-    for no, similarity in similarity_index.items():
+    for key, similarity in similarity_index.items():
         if len(top_indexes) >= top_k:
             break
         else:
-            top_indexes.append(data.index[data['no'] == no].tolist()[0])
+            court, no = key.split('#')
+            top_indexes.append(data.index[(data['court'] == court) & (data['no'] == no)].tolist()[0])
 
     matches = data.loc[top_indexes]
     del matches['judgement']
-    matches.to_csv('bert_results/bert_{}.csv'.format(query['no']), index=False)
+    key_name = query['court'] + '#' + query['no']
+    matches.to_csv('bert_results/bert_{}.csv'.format(key_name), index=False)
     return matches
 
 
@@ -162,11 +197,13 @@ def recommend_similar(train, test):
     for idx in tqdm(range(len(test))):
         query = {
             'no': test['no'][idx],
+            'court': test['court'][idx],
             'reason': test['reason'][idx],
             'new_reason': literal_eval(test['new_reason'][idx]),
             'issue': literal_eval(test['new_relatedIssues'][idx]),
             'truth': test['truth'][idx],
             'penalty': test['maxpenalty'][idx],
+            # 'parse_rank': test['parse_rank50'][idx],
             'TOPK': config['TOPK']
         }
 
